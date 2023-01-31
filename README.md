038c1f181c8e7c32441f42cd9a4a12de008381f3
rensilva5 <renzoasilva@gmail.com>
2023-01-30 20:39:44 -0500

    Cleaning code and connecting backEnd with frontEnd

--- README.md
+++ README.md
@@ -14,10 +14,21 @@ So we can see the app is running.
 Creating conections to get status and database
 Get JSON file (JavaScript Object Notation) by fectching.
 
+## Create local server next // Test mode, then we move on
+npx json-server --watch data/db.json --port 8000
+
 ## We will be working on development mode
 
 Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Open (http://localhost:3000) to view it in your browser.
 
-## Create local server
-npx json-server --watch data/db.json --port 8000
+And open the backEnd with (http://localhost:3002) to view it in your browser.
+
+## Connect backEnd with mongodb
+Then use node or modenon to keep runnig the local
+
+## Front End
+Clean area and work on App. Embedded in index.js to render it.
+
+## Connect Back End with front End
+
--- src/App.js
+++ src/App.js
@@ -1,59 +1,32 @@
-// import { useEffect } from 'react'; // pending to be used.
-import { useEffect, useState } from 'react';
-import './App.css';
-// import Device from './data';
-// import PostList from './posts/PostList'; // second option to fetch for data
+import { useEffect, useState } from "react";
 
 function App() {
-  const [deviceData, setDeviceData] = useState([])
+  const [deviceData, setDeviceData] = useState([]);
 
-  // const postCollectorRef = collection(db, 'json/') / pending to be used.
   // const handleData = () => {
   //   console.log("Hello world")
   // }
-  useEffect(() => {
-    // fetch("http://192.168.15.35/#/json")
-    fetch("http://192.168.15.35/rpc/Switch.GetStatus?id=0")
-    // fetch("http://localhost:3002/devices")
-    
-    .then(res => {
-      return res.json()
-    })
-    .then((data) => {
-      console.log(data)
-      // setDeviceData(data[0].name)
-    })
-  }, [])
-  //   fetch("http://192.168.15.35/#/json", {  // http://192.168.15.35/rpc/Shelly.GetStatus
-  //     method: "GET",
-  //     headers: {
-  //       'Accept': 'application/json'
-  //     },
-  //   })
-  //   .then(response => response.json())
-  //   .then(response =>
-  //     console.log(JSON.stringify(response)))
-  // }
 
-//or
-  
-// useEffect(() => {
-//   const getJson = async () => {
-//     const data = await getDocs(postCollectorRef)
-//     setDataList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id})))
-//   };
-// })
+  useEffect(() => {
+    fetch("http://192.168.15.35/rpc/Switch.GetStatus?id=0")
+      // fetch("http://localhost:3002/devices")
+
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // setDeviceData(data[0].name)
+      });
+  }, []);
 
   return (
     <div className="App">
       {/* {posts && <PostList posts={posts} title="all posts" />} */}
       {/* {deviceData && <deviceDataPostList posts={posts} title="all posts" />} */}
-      {/* below part of the second option */}
-      {/* <PostList /> */}
-      {/* <h1>Test</h1> */}
-      {deviceData ? ( <h1>{deviceData}</h1> ) : (<h1>Loading</h1>)}
-      
-</div>
-  )}
+      {deviceData ? <h1>{deviceData}</h1> : <h1>Loading</h1>}
+    </div>
+  );
+}
 
 export default App;
